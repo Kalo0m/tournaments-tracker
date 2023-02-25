c9daa8fef7035dd0ceb2fdcf4f8f000ea5e21bb4
Théo Letouzé <theo.letouze44@gmail.com>
2023-02-25 16:43:15 +0100

    test

--- packages/find-tournaments/index.ts
+++ packages/find-tournaments/index.ts
@@ -4,9 +4,8 @@ import type { schema } from './types';
 type Tournaments = z.infer<typeof schema>;
 
 export async function findTournaments() {
-  // const data = await fetchTournaments();
-  // console.log(data);
-  // // schema.parse(data);
-  // return data[3] as Tournaments;
-  return [];
+  const data = await fetchTournaments();
+  console.log(data);
+  // schema.parse(data);
+  return data[3] as Tournaments;
 }
