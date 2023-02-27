15e759ea9f8e0dbab60e7040a7ee5634f68dfb43
root <root@DESKTOP-QCTARS9.localdomain>
2023-02-27 23:33:23 +0100

    fix db

--- packages/test-next/next.config.js
+++ packages/test-next/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   experimental: {
     appDir: true,
-    serverComponentsExternalPackages: ['@prisma/client'],
+    serverComponentsExternalPackages: ['database', 'prisma'],
   },
 };
 
