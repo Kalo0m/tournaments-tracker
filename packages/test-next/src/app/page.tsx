cd4102e48b040bebcc56361055edb7ce6ae9410b
root <root@DESKTOP-QCTARS9.localdomain>
2023-02-27 23:12:36 +0100

    v2

--- packages/test-next/src/app/page.tsx
+++ packages/test-next/src/app/page.tsx
@@ -1,3 +1,17 @@
+import Form from '@/components/form';
+import './globals.css';
 export default async function Home() {
-  return <main>gregregr</main>;
+  return (
+    <main className="bg-slate-900 p-4 text-white flex flex-col justify-center items-center h-screen">
+      <h1 className="text-3xl lg:text-5xl mb-2 max-w-5xl font-bold text-center text-blue-500">
+        Soit le premier à t'inscrire aux tournois de{' '}
+        <span className="text-yellow-300">Nantes</span>
+      </h1>
+      <p className="text-slate-500 text-center mb-7 lg:text-base text-sm">
+        Nous t'enverrons un mail quand un tournoi sera disponible dans la région
+        Nantaise
+      </p>
+      <Form />
+    </main>
+  );
 }
--- packages/test-next/src/app/test/route.ts
+++ packages/test-next/src/app/test/route.ts
@@ -1,7 +1,23 @@
 import { PrismaClient } from 'database';
 
-export async function GET(request: Request) {
+export async function POST(request: Request) {
   const db = new PrismaClient();
+  console.log(request.body);
+  await db.user.create({
+    data: {
+      email:
+        (await request.body
+          ?.getReader()
+          .read()
+          .then((value) => new TextDecoder().decode(value.value) as string)) ??
+        '',
+    },
+  });
   console.log(await db.user.findMany());
   return new Response('Hello world!', { status: 200 });
 }
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
