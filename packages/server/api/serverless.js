76506627972af323368fe1ebcacc0aa6fe3c2acd
root <root@DESKTOP-QCTARS9.localdomain>
2023-03-01 19:38:03 +0100

    test vercel with fastify

--- packages/server/api/serverless.js
+++ packages/server/api/serverless.js
@@ -13,7 +13,7 @@ const app = Fastify({
 });
 
 // Register your application as a normal plugin.
-app.register(import('../'));
+app.register(import('../index'));
 
 export default async (req, res) => {
   await app.ready();
