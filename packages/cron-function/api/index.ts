c843efa0694bcf08adbfec559508b3678206ce3d
Théo Letouzé <theo.letouze44@gmail.com>
2023-03-02 09:49:22 +0100

    await promise all

--- packages/cron-function/api/index.ts
+++ packages/cron-function/api/index.ts
@@ -1,7 +1,6 @@
 import type { Prisma } from 'database';
 import { PrismaClient } from 'database';
 import { findTournaments } from 'find-tournaments';
-import { isBuffer } from 'util';
 
 // eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires, unicorn/prefer-module
 const Mailjet = require('node-mailjet');
@@ -33,63 +32,69 @@ export default async function handler(_: any, res: any) {
   );
 
   const users = await db.user.findMany({ select: { email: true } });
-  console.log(users);
   if (newTournaments.length > 0) {
-    for (const user of users) {
-      void mailjet.post('send', { version: 'v3.1' }).request({
-        Messages: [
-          {
-            From: {
-              Email: 'theo.letouze44@gmail.com',
-              Name: 'Théo Letouzé',
-            },
-            To: [
-              {
-                Email: user.email,
-                Name: user.email,
+    console.info('new tournaments');
+    await Promise.all(
+      users.map((user) => {
+        return mailjet.post('send', { version: 'v3.1' }).request({
+          Messages: [
+            {
+              From: {
+                Email: 'theo.letouze44@gmail.com',
+                Name: 'Théo Letouzé',
               },
-            ],
-            Subject: 'Nouveau tournoi disponible !',
-            TextPart:
-              'Bonjour, un ou plusieurs tournois sont disponibles sur Tenup',
-            HTMLPart: `<h3>Voici la liste des nouveaux tournois :</h3><ul>${newTournaments
-              .map(
-                (tournament) =>
-                  `<li>${tournament.name} - ${tournament.city}, dates : ${
-                    tournament.startDate.toString().split(' ')[0]
-                  } - ${tournament.endDate.toString().split(' ')[0]}</li>`
-              )
-              .join('')}</ul>`,
-          },
-        ],
-      });
-    }
+              To: [
+                {
+                  Email: user.email,
+                  Name: user.email,
+                },
+              ],
+              Subject: 'Nouveau tournoi disponible !',
+              TextPart:
+                'Bonjour, un ou plusieurs tournois sont disponibles sur Tenup',
+              HTMLPart: `<h3>Voici la liste des nouveaux tournois :</h3><ul>${newTournaments
+                .map(
+                  (tournament) =>
+                    `<li>${tournament.name} - ${tournament.city}, dates : ${
+                      tournament.startDate.toString().split(' ')[0]
+                    } - ${tournament.endDate.toString().split(' ')[0]}</li>`
+                )
+                .join('')}</ul>`,
+            },
+          ],
+        });
+      })
+    );
 
     await db.tournament.createMany({
       data: newTournaments,
     });
   } else {
-    for (const user of users) {
-      void mailjet.post('send', { version: 'v3.1' }).request({
-        Messages: [
-          {
-            From: {
-              Email: 'theo.letouze44@gmail.com',
-              Name: 'Théo Letouzé',
-            },
-            To: [
-              {
-                Email: user.email,
-                Name: user.email,
+    console.info('no new tournaments');
+
+    await Promise.all(
+      users.map((user) => {
+        return mailjet.post('send', { version: 'v3.1' }).request({
+          Messages: [
+            {
+              From: {
+                Email: 'theo.letouze44@gmail.com',
+                Name: 'Théo Letouzé',
               },
-            ],
-            Subject: 'Pas de nouveau tournoi disponible !',
-            HTMLPart:
-              "<p>Bonjour, aucun tournoi n'est disponible sur Tenup pour le moment, peut-être demain ...</p>",
-          },
-        ],
-      });
-    }
+              To: [
+                {
+                  Email: user.email,
+                  Name: user.email,
+                },
+              ],
+              Subject: 'Pas de nouveau tournoi disponible !',
+              HTMLPart:
+                "<p>Bonjour, aucun tournoi n'est disponible sur Tenup pour le moment, peut-être demain ...</p>",
+            },
+          ],
+        });
+      })
+    );
   }
 
   res.status(200).json({ message: 'sent' });
